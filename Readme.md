8102d936d8ce2620f96df58766e6bdd99848eee8
sarthak.jha <sarthak.jha@arista.com>
2023-02-26 21:54:30 +0530

    project init

---
+++ Readme.md
@@ -0,0 +1,5 @@
+# Aero-AI
+
+This is going to be an airport simulator written in C++
+
+To improve our system's accuracy and efficiency, i'm planning to replace a deterministic algorithm with a machine learning model. Our current focus is on assessing the strengths and limitations of the existing algorithm so that we can design a suitable ML model to replace it
---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cmath>
+#include <vector>
+#include <string>
+#include <numeric>
+
+#define REP(i, a, b) for (int i = (a); i <= (b); i++)
+#define REP_Dec(i, a, b) for (int i = (a); i >= (b); i--)
+#define FOR(i, n) for (int i = 0; i < n; i++)
+#define FOR1(i, n) for (int i = 1; i <= n; i++)
+#define print(n) cout << n << endl
+#define mp make_pair
+#define pb push_back
+#define eb emplace_back
+
+using namespace std;
+
+typedef long long int ll;
+const int inf = 1e9;
+const ll N = 1000000000000L;
+
+int main()
+{
+    print("Project init");
+
+        return 0;
+}
